4e77211a7cc7cd5778c5117c8bed691c163e7637
Joey <20105823@setu.ie>
2025-12-13 16:06:02 +0000

    Initial Express App

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,34 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/tasks/index.js';
-import './db/index.js';
 
 dotenv.config();
-
-const errHandler = (err, req, res, next) => {
-  /* if the error in development then send stack trace to display whole error,
-  if it's in production then just send error message  */
-  if(process.env.NODE_ENV === 'production') {
-    return res.status(500).send(`Something went wrong!`);
-  }
-  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
-};
 
 const app = express();
 
 const port = process.env.PORT;
 
-// Parse JSON request bodies
-app.use(express.json());
-
-// Serve static files (optional for homepage)
 app.use(express.static('public'));
 
-// Mount tasks API routes
-app.use('/api/tasks', tasksRouter);
-
-app.use(errHandler);
-
 app.listen(port, () => {
-  console.info(`Server running at http://localhost:${port}`);
+  console.info(`Server running at ${port}`);
 });
